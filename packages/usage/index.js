b6a1bb84cfaeaa1f530d116fa15738ef37c6a7ed
Jayant Malik <dev.jayantmalik@gmail.com>
2021-02-20 16:33:20 +0530

    fix: typo in message

--- packages/usage/index.js
+++ packages/usage/index.js
@@ -2,4 +2,4 @@ const alpha = require("@dcourses/alpha");
 const beta = require("@dcourses/beta");
 
 const result = alpha() + " and " + beta();
-console.log("It's :" + result);
+console.log("It's " + result);
